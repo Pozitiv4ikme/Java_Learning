8c25848ff90c2fc2552167d2cb73a659278a145e
Vitaliy Pashynskyi <vitaliy.pashynskyi@gmail.com>
2022-04-25 13:06:57 +0200

    deleted extra comments

--- src/ua/lviv/iot/firstProject/LawnMower.java
+++ src/ua/lviv/iot/firstProject/LawnMower.java
@@ -4,34 +4,18 @@ public record LawnMower(int grassLevel, int enginePower, double fuelTankVolume, 
 						String developerCompany, int yearOfManafacture) {
 	
 	static public final String TYPE = "GrassKiller";
-	//static public final int amount = 3;
-	//private double ratingOfProduct;
-	//private String name;
 
 	public LawnMower() 
 	{
 		this(3, 12, 3.2d, "Out of town", "Bosh", 2019);
 	}
 
-	public LawnMower(int grassLevel, int enginePower, double fuelTankVolume)
-	{
+	public LawnMower(int grassLevel, int enginePower, double fuelTankVolume) {
 		this(grassLevel, enginePower, fuelTankVolume, "Z560X", "Husqvarna", 2022);
-	}
-
-	public LawnMower(int grassLevel, int enginePower, double fuelTankVolume, String model,
-					 String developerCompany, int yearOfManafacture)
-	{
-		this.grassLevel = grassLevel;
-		this.enginePower = enginePower;
-		this.fuelTankVolume = fuelTankVolume;
-		this.model = model;
-		this.developerCompany = developerCompany;
-		this.yearOfManafacture = yearOfManafacture;
 	}
 	
 	@Override
-	public String toString() 
-	{
+	public String toString() {
 		return "Grass mowing width in centimeters - " + grassLevel + ", "
 				+ "engine power in watts - " + enginePower + ", model - " + model +
 				", fuel tank volume - " + fuelTankVolume + ", developer company is "
@@ -42,8 +26,4 @@ public record LawnMower(int grassLevel, int enginePower, double fuelTankVolume, 
 	{
 		return "Type of lawn-mower - " + TYPE;
 	}
-
-	//public static int getStaticFieldAmount() { return "Amount of all product is " + amount; }
-	//public double setRatingOfProduct() { this.rating = rating; }
-	//public String getThanks() { return "Thank you - " + name + " for purchasing the product in our store!"}
 }
--- src/ua/lviv/iot/firstProject/Main.java
+++ src/ua/lviv/iot/firstProject/Main.java
@@ -1,54 +1,17 @@
 package ua.lviv.iot.firstProject;
 
-import java.util.ArrayList;
-import java.util.List;
-
 public class Main {
 
 	public static void main(String[] args) {
 		
-		LawnMower bosh = new LawnMower();
-		LawnMower husqvarna = new LawnMower(1, 24, 10.6);
-		LawnMower karcher = new LawnMower(4, 18, 5, "LMO 18-33 Battery Set EU", "KA'RCHER", 2020);
-
-		//List<LawnMower> listOfProduct = new ArrayList<>();
-		//listOfProduct.add(bosh);
-		//listOfProduct.add(husqvarna);
-		//listOfProduct.add(karcher);
-
-		//int i = 0;
-		//while(i < listOfProduct.size()) {
-		//	System.out.println(listOfProduct.get(i));
-		//	i++;
-		//}
+		LawnMower Bosh = new LawnMower();
+		LawnMower Husqvarna = new LawnMower(1, 24, 10.6);
+		LawnMower Karcher = new LawnMower(4, 18, 5, "LMO 18-33 Battery Set EU", "KA'RCHER", 2020);
 
 		System.out.println(LawnMower.getStaticField());
-		System.out.println(bosh);
-		System.out.println(husqvarna);
-		System.out.println(karcher);
+		System.out.println(Bosh);
+		System.out.println(Husqvarna);
+		System.out.println(Karcher);
 	}
-
-	//int firstNumber = 10;
-	//int secondNumber = 20;
-	//int thirdNumber = 20;
-
-	//if(firstNumber > secondNumber) {
-	//	System.out.println("The first number is greater than the second");
-	// }
-	//else {
-	//	System.out.println("The second number is greater than the first");
-	// }
-
-	//or
-
-	//if(secondNumber == thirdNumber) {
-	//	System.out.println("The numbers are the same");
-	// }
-	//else if(firstNumber > secondNumber) {
-	//	System.out.println("The first number is greater than the second");
-	// }
-	//else {
-	//	System.out.println("The second number is greater than the first");
-	// }
 }
 
